11ff8336a2d9288c1d78dbc30134e7f6f0d9ab33
zahid <epiczahid@gmail.com>
2021-12-28 21:30:19 +0600

    getting ready for private playlists

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -4,7 +4,7 @@ export default function Welcome({ username }) {
 	return (
 		<div>
 			<h1 className="welcome_text">Welcome, {username}</h1>
-			<p className="welcome_desc">Not seeing your playlist? Make sure its public!</p>
+			{/* <p className="welcome_desc">Not seeing your playlist? Make sure its public!</p> */}
 		</div>
 	);
 }
